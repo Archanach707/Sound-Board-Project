3a5b6712e72c2f7f872f9a3c770c7f6fec1c0038
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-28 16:04:33 +0530

    1. Add button in html file, after that 2.jump to js file

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sounds => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+  
+})
