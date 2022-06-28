25115d9e68850a1877f6f0a1d62ba3b7014cfa4d
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-28 16:18:57 +0530

    1. Audio element create as like button by js

--- js/index.js
+++ js/index.js
@@ -3,5 +3,10 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 sounds.forEach(sounds => {
   const btn = document.createElement('button')
   btn.classList.add('btn')
-  
+
+  btn.innerText = sounds
+
+  document.getElementById('buttons').
+  appendChild(btn)
+
 })
